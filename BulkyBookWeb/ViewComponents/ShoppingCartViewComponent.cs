dded911d5908f2ded07b470dee8b49b3387c15d0
Andre Sernajoto <andre.sernajoto@gmail.com>
2022-08-05 18:53:08 -0300

    added shopping cart view component

---
+++ BulkyBookWeb/ViewComponents/ShoppingCartViewComponent.cs
@@ -0,0 +1,43 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Utility;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace BulkyBookWeb.ViewComponents
+{
+    public class ShoppingCartViewComponent : ViewComponent
+    {
+        private readonly IUnityOfWork _unityOfWork;
+
+        public ShoppingCartViewComponent(IUnityOfWork unityOfWork)
+        {
+            _unityOfWork = unityOfWork;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var claim = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier);
+
+            if (claim != null)
+            {
+                if (HttpContext.Session.GetInt32(SD.SessionCart) != null)
+                {
+                    return View(HttpContext.Session.GetInt32(SD.SessionCart));
+                }
+                else
+                {
+                    HttpContext.Session.SetInt32(SD.SessionCart,
+                        _unityOfWork.ShoppingCart.GetAll(x => x.ApplicationUserId == claim.Value).ToList().Count);
+
+                    return View(HttpContext.Session.GetInt32(SD.SessionCart));
+                }
+            }
+            else
+            {
+                HttpContext.Session.Clear();
+                return View(0);
+            }
+        }
+    }
+}
